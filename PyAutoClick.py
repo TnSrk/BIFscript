f6c429f36f7a496491b54422456d7b9736dd6d2c
tnsrk <tanawut.cool@gmail.com>
2025-04-09 13:56:41 +0700

    add province

--- PyAutoClick.py
+++ PyAutoClick.py
@@ -6,6 +6,8 @@ import csv
 import random
 from datetime import datetime
 
+ProvinceL = "Bangkok,Amnat Charoen,Ang Thong,Bueng Kan,Buriram,Chachoengsao,Chai Nat,Chaiyaphum,Chanthaburi,Chiang Mai,Chiang Rai,Chonburi,Chumphon,Kalasin,Kamphaeng Phet,Kanchanaburi,Khon Kaen,Krabi,Lampang,Lamphun,Loei,Lopburi,Mae Hong Son,Maha Sarakham,Mukdahan,Nakhon Nayok,Nakhon Pathom,Nakhon Phanom,Nakhon Ratchasima,Nakhon Sawan,Nakhon Si Thammarat,Nan,Narathiwat,Nong Bua Lamphu,Nong Khai,Nonthaburi,Pathum Thani,Pattani,Phang Nga,Phatthalung,Phayao,Phetchabun,Phetchaburi,Phichit,Phitsanulok,Phra Nakhon Si Ayutthaya,Phrae,Phuket,Prachinburi,Prachuap Khiri Khan,Ranong,Ratchaburi,Rayong,Roi Et,Sa Kaeo,Sakon Nakhon,Samut Prakan,Samut Sakhon,Samut Songkhram,Saraburi,Satun,Sing Buri,Sisaket,Songkhla,Sukhothai,Suphan Buri,Surat Thani,Surin,Tak,Trang,Trat,Ubon Ratchathani,Udon Thani,Uthai Thani,Uttaradit,Yala,Yasothon,Other,Ayutthaya".split(",")
+
 def RS(T=2):
 	t.sleep((random.randrange(1,T)*10.0)/10.0)
 
@@ -15,7 +17,17 @@ def wtab(InStr, E=False):
 		p.press('enter')
 	RS()
 	p.press('tab')	
-	
+
+def ThProvincer(InStr="Other"):
+
+	if InStr in ProvinceL:
+		wtab(InStr,E=True)
+	else:
+		p.press('up',1)
+		t.sleep(0.5)
+		p.write(InStr)
+		p.press('enter')
+		p.press('tab')	
 
 def wchoice(InNum,Tab=True):
 	p.press('space')
